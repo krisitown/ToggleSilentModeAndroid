1dfaadd89f7ab2b0e70fb8ac6e478b738f0ca38c
krisitown <krisipwn@gmail.com>
2016-03-18 01:09:06 +0200

    First commit

---
+++ app/src/main/java/com/training/krisitown/androidtestingapp/MainActivity.java
@@ -0,0 +1,103 @@
+package com.training.krisitown.androidtestingapp;
+
+import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //creating an instance
+    AudioManager audioManager;
+    //creating an instance of the image
+    ImageView imageView;
+
+    //creating instances of the images we will be using
+    Drawable silentImage;
+    Drawable notSilentImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        imageView = (ImageView)findViewById(R.id.audioModeImage);
+        silentImage = getResources().getDrawable(R.drawable.phone_silent);
+        notSilentImage = getResources().getDrawable(R.drawable.phone_on);
+
+
+        //creating the toggle button and adding a listener to it
+        Button toggle = (Button)findViewById(R.id.toggle);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPhoneSilent()){
+                    audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                }
+                else {
+                    audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                }
+
+                toggleUi();
+            }
+        });
+    }
+
+    //checks if the phone is in silent mode
+    private boolean isPhoneSilent(){
+        int ringerMode = audioManager.getRingerMode();
+        return ringerMode == AudioManager.RINGER_MODE_SILENT;
+    }
+
+    private void toggleUi(){
+        if(isPhoneSilent()){
+            imageView.setImageDrawable(silentImage);
+        }
+        else {
+            imageView.setImageDrawable(notSilentImage);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
